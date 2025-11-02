51cdf1992cf1aa143b024b14dc6c2d0c4672eecf
venelinpetrov <vpetrov33tu@gmail.com>
2025-11-02 12:50:31 +0200

    Fix local development

--- db.js
+++ db.js
@@ -2,17 +2,28 @@ import mysql from "mysql2/promise";
 
 const instanceConnection = process.env.INSTANCE_CONNECTION;
 
-const pool = mysql.createPool({
+const sharedConfig = {
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
-
-	// Cloud SQL Unix socket
-	socketPath: `/cloudsql/${instanceConnection}`,
-
 	waitForConnections: true,
 	connectionLimit: 5,
 	queueLimit: 0,
-});
+};
+
+let pool;
+
+if (process.env.NODE_ENV === "development") {
+	pool = mysql.createPool({
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		...sharedConfig,
+	});
+} else {
+	pool = mysql.createPool({
+		socketPath: `/cloudsql/${instanceConnection}`,
+		...sharedConfig,
+	});
+}
 
 export default pool;
